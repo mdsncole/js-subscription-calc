02ff5a56ca237615748b1e8adc1fa25c20089f67
madison <mdsncole97@gmail.com>
2023-07-25 15:22:17 -0500

    Added event listeners and functions

--- js/script.js
+++ js/script.js
@@ -4,3 +4,26 @@ var subDurationElement = document.querySelector("#months");
 var result = document.querySelector(".result");
 var subType = "basic";
 var subDuration = 1;
+
+subTypeElement.addEventListener("change", function (e) {
+    subType = e.target.value
+    updateSubscriptionDiv();
+});
+
+subDurationElement.addEventListener("change", function (e) {
+    subDuration = Number(e.target.value)
+    updateSubscriptionDiv();
+});
+
+var updateSubscriptionDiv = function () {
+    var monthlyCost = 5;
+
+    if (subType === "standard") {
+        monthlyCost = 7;
+    } else if (subType === "premium") {
+        monthlyCost = 10;
+    }
+
+    var total = subDuration * monthlyCost;
+    result.innerText = `You chose ${subDuration} month ${monthlyCost} plan, for a total of ${total} `
+}
